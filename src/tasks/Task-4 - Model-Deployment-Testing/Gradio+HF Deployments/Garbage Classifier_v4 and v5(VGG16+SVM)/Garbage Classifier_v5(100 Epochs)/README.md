7980e31b8c80174fb8b20d58b20ebaded7b14dbb
2Much2Code:) <106218100+AdiNarendra98@users.noreply.github.com>
2022-08-12 13:24:33 +0530

    commit

---
+++ src/tasks/Task-4 - Model-Deployment-Testing/Gradio+HF Deployments/Garbage Classifier_v4 and v5(VGG16+SVM)/Garbage Classifier_v5(100 Epochs)/README.md
@@ -0,0 +1,13 @@
+---
+title: Garbage Classifer V5
+emoji: 🚮
+colorFrom: yellow
+colorTo: indigo
+sdk: gradio
+sdk_version: 3.1.4
+app_file: app.py
+pinned: false
+python_version: 3.7.6
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ src/tasks/Task-4 - Model-Deployment-Testing/Gradio+HF Deployments/Garbage Classifier_v4 and v5(VGG16+SVM)/Garbage Classifier_v5(100 Epochs)/app.py
@@ -0,0 +1,31 @@
+import gradio as gr
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import tensorflow.keras as keras
+import keras.applications.vgg16 as vgg16
+
+from tensorflow.keras.models import load_model
+ 
+# load model
+model = load_model('model9200.h5')
+
+classnames = ['battery','cardboard','clothes','food','glass','medical','metal','paper','plastic','shoes']
+
+
+
+def predict_image(img):
+  img_4d=img.reshape(-1,224, 224,3)
+  prediction=model.predict(img_4d)[0]
+  return {classnames[i]: float(prediction[i]) for i in range(10)}
+
+                
+
+image = gr.inputs.Image(shape=(224, 224))
+label = gr.outputs.Label(num_top_classes=3)
+article="<p style='text-align: center'>Made by Aditya Narendra with 🖤</p>"
+
+
+
+gr.Interface(fn=predict_image, inputs=image,  title="Garbage Classifier V5-VGG16+SVM",
+    description="This is a Garbage Classification Model Trained using VGG16+SVM(100 Epochs).Deployed to Hugging Faces using Gradio.",outputs=label,article=article,enable_queue=True,interpretation='default').launch(share="True")
