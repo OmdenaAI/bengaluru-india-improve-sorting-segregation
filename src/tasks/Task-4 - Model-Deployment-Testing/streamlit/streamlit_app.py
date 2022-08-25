5cd754983435949fcc7602e1d534cc1da64c8519
nandarLMA <73601219+NandarLMA@users.noreply.github.com>
2022-08-25 16:56:56 +0630

    Update streamlit_app.py

--- src/tasks/Task-4 - Model-Deployment-Testing/streamlit/streamlit_app.py
+++ src/tasks/Task-4 - Model-Deployment-Testing/streamlit/streamlit_app.py
@@ -4,7 +4,11 @@ import cv2 as cv
 import numpy as np
 from PIL import Image
 import pandas as pd
-from tensorflow.keras.models import load_model
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization
+import keras.applications.vgg16 as vgg16
+from keras.regularizers import l2
 
 st.set_page_config(page_title="Image Segmentation",
                    page_icon="📝", layout="wide")
@@ -16,11 +20,12 @@ def main():
     show_image = st.container()
     image = None
     #img_size = 512
-    width, height, channel = 298, 384, 3
+    width, height, channel = 224, 224, 3
     predict = False
-    model = load_model('DS11Sudhanva.h5')
-    labels = ['cardboard','metal','paper','plastic','trash','green-glass','white-glass','brown-glass','clothes',
-    'biological','battery','shoes']
+    model = load_model('model')
+
+    labels = ['battery','cardboard','clothes','food','glass','medical','metal','paper','plastic','shoes']
+
     with header:
         st.title("Waste Classification Project")
 
@@ -32,6 +37,10 @@ def main():
             "sorting and segregation process of solid waste management. It comes under " 
             "SDG goals of 3 (Good Health & Well Being), 6(Clean Water & Sanitation) and "
             "11(Sustainable Cities & Communities)."
+        )
+        st.markdown(
+            "10 types of waste can be classified in this project - battery, cardboard, clothes, food," 
+            "glass, medical, metal, paper, plastic and shoes." 
         )
 
     with upload_image:
@@ -48,17 +57,14 @@ def main():
     if predict == True:
         image = Image.open(image)
         image = np.array(image.convert("RGB"))
-
         image = cv.resize(image, (width, height))
         img = image.reshape(-1, width, height, channel)
-        #img = img/255
-        val = model.predict(img)
+        pred = model.predict(img)[0]
         _, col2, _ = st.columns([2,6,2])
         with col2:
-            st.subheader(f'output: {labels[np.argmax(val[0])]}')
+            st.subheader(f'output: {labels[np.argmax(pred)]}')
             chart_data = pd.DataFrame(
-                val[0],
-                index=labels)
+                pred,index=labels)
             st.bar_chart(chart_data)
 
 if __name__ == "__main__":
